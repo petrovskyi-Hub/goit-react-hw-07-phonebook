b90c52f32b1beaa0674c0f7e15db9785535dc9be
Yaroslav Petrovskyi <yarikpetrovsk@gmail.com>
2021-01-24 17:01:45 +0200

    Добавил мемоизацию

--- src/redux/Contacts/contacts-selectors.js
+++ src/redux/Contacts/contacts-selectors.js
@@ -1,13 +1,27 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
+export const getFilter = state => state.contacts.filter;
 
-export const getFilteredContacts = state => {
-  const contacts = getContacts(state);
-  const filter = state.contacts.filter;
-  const normalizedFilter = filter.toLowerCase();
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
 
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
+
+// export const getFilteredContacts = state => {
+//   const contacts = getContacts(state);
+//   const filter = state.contacts.filter;
+//   const normalizedFilter = filter.toLowerCase();
+
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
 
 export const getIsLoading = state => state.contacts.isLoading;
